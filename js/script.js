5967107d9122c47fe8cff4dc21a9eb61740abbe8
Njiraini23 <njiraini88@gmail.com>
2023-12-27 17:22:05 +0300

    Final project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===typing animation===*/
+var typed = new Typed(".typing",{
+     strings:["","Web Designer","Backend Software Engineer","Technical Writer", "Agribusiness Consultant"],
+     typeSpeed:100,
+     BackSpeed:60,
+     loop:true
+})
